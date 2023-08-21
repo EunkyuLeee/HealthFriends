d55ad645a03cc7ed7f1ba5070a13dab9e89d5154
joon <yijiyoon06@gmail.com>
2023-08-21 23:32:29 +0900

    시간순으로 정렬된 랭킹 조회 컨트롤러

--- HealthFriends/src/main/java/com/example/HealthFriends/controller/ExerciseController.java
+++ HealthFriends/src/main/java/com/example/HealthFriends/controller/ExerciseController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -86,4 +83,8 @@ public class ExerciseController {
         return exerciseService.getExerciseType();
     }
 
+    @GetMapping("/api/ranking")
+    public List<String> sortedList(@RequestParam Long exNo) {
+       return exerciseService.sortedListByTime(exNo);
+    }
 }
