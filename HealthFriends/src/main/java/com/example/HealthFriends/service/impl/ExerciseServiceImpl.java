b759d83325ccca13027f776aac90fdcfe52b0aa4
joon <yijiyoon06@gmail.com>
2023-08-21 23:31:42 +0900

    시간 순으로 정렬된 리스트 서비스 메서드

--- HealthFriends/src/main/java/com/example/HealthFriends/service/impl/ExerciseServiceImpl.java
+++ HealthFriends/src/main/java/com/example/HealthFriends/service/impl/ExerciseServiceImpl.java
@@ -12,7 +12,10 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 @Service
 public class ExerciseServiceImpl implements ExerciseService {
@@ -108,5 +111,33 @@ public class ExerciseServiceImpl implements ExerciseService {
         }
 
         return dtoList;
+    }
+
+    @Override
+    public List<String> sortedListByTime(Long exNo){
+        List<ExerciseRecordData> entities = jpaExerciseRepository.findByExerciseNo(exNo);
+        List<ExerciseRecordDto> dtos = new ArrayList<>();
+
+        for(ExerciseRecordData ed : entities){
+            ExerciseRecordDto dto = new ExerciseRecordDto();
+            dto.setExerciseTime(ed.getExerciseTime());
+            dtos.add(dto);
+        }
+
+        PriorityQueue<String> heap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0; i < dtos.size(); i++){
+            if(dtos.get(i).getExerciseTime() != null){
+                heap.add(dtos.get(i).getExerciseTime());
+            }
+        }
+        for(int i = 0; i < dtos.size(); i++){
+            dtos.get(i).setExerciseTime(heap.poll());
+        }
+
+        List<String> sortedList = dtos.stream().map(ExerciseRecordDto::getExerciseTime).collect(Collectors.toList());
+
+        return sortedList;
+
     }
 }
