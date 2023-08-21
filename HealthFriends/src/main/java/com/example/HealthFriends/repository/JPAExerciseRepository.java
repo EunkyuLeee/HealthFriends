b4c62499bb0c6c7056add505acf3c1bfc7476060
joon <yijiyoon06@gmail.com>
2023-08-21 23:30:07 +0900

    exerciseNo를 통한 리스트 조회 메서드 추가

--- HealthFriends/src/main/java/com/example/HealthFriends/repository/JPAExerciseRepository.java
+++ HealthFriends/src/main/java/com/example/HealthFriends/repository/JPAExerciseRepository.java
@@ -1,7 +1,12 @@
 package com.example.HealthFriends.repository;
 
+import com.example.HealthFriends.dto.ExerciseRecordDto;
 import com.example.HealthFriends.entity.ExerciseRecordData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface JPAExerciseRepository extends JpaRepository<ExerciseRecordData, Long> {
+    List<ExerciseRecordData> findByExerciseNo(Long exNo);
 }
