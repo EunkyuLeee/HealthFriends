5f13515339cbe3c2c771ac87e7883d593c026c04
dayeon0919 <rdy020919@naver.com>
2023-09-04 01:39:13 +0900

    commit message

--- HELF/src/screens/sign_up.tsx
+++ HELF/src/screens/sign_up.tsx
@@ -1,24 +1,56 @@
-import { Text, View, StyleSheet, TextInput } from 'react-native'
+import React, {useState} from 'react'
+import { Text, View, StyleSheet, TextInput, Alert, Button } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { MD2Colors as Colors } from 'react-native-paper'
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Picker } from '@react-native-picker/picker';
+import RNPickerSelect from 'react-native-picker-select';
+import { AsyncStorage } from 'react-native';
 
 export default function SignUp({navigation} : any) {
+    const [pickerValue, setPickerValue] = useState("1");
+    const [ value, setValue] = useState('ID 중복 확인을 해주세요.');
   return (
     <View style={[styles.view]}>
       <View>
 
-        <View style={styles.title}>
-            <Text style={styles.bold}>REGISTER TO</Text>
-            <Text style={styles.regular}>HELF Project</Text>
+        <View>
+          <TextInput style={styles.textInput_id} placeholder="ID"/>
+        </View>
+
+        <View style={styles.instruction}>
+
+            <Text>{value}</Text>
+            <Button title="change Value" onPress={() => setValue('사용가능한 ID입니다.')}/>
+
         </View>
 
         <View>
-          <TextInput style={styles.textInput_1} placeholder="Email"/>
+          <TextInput style={styles.textInput_pw} placeholder="PASSWORD"/>
+        </View>
+
+        <View>
+          <TextInput style={styles.textInput_pw} placeholder="PASSWORD CHECK"/>
+        </View>
+
+        <View>
+           <TextInput style={styles.textInput_name} placeholder="NAME"/>
+        </View>
+
+        <View>
+          <TextInput style={styles.textInput_name} placeholder="E-MAIL"/>
+        </View>
+
+        <View>
+          <TextInput style={styles.textInput_name} placeholder="전화번호"/>
+        </View>
+
+        <View>
+          <TextInput style={styles.textInput_name} placeholder="생년월일"/>
         </View>
 
         <TouchableOpacity>
-          <Text style={styles.button} onPress = {()=>navigation.navigate('SignUp_Password')}> NEXT  
+          <Text style={styles.button} onPress = {()=>Alert.alert('회원가입이 완료되었습니다.')}> 가입하기
               <Icon name = 'arrow-right' size = {20}/> 
           </Text>
         </TouchableOpacity>
@@ -35,18 +67,29 @@ export default function SignUp({navigation} : any) {
 const styles = StyleSheet.create({
   view: {backgroundColor: Colors.grey200, flex: 1, justifyContent: 'center'},
 
-  title: {fontSize: 30, fontFamily: 'SEBANG_Gothic_Bold', textAlign:'center', marginBottom: 20, flexDirection: 'column'},
-  bold: {color: Colors.blue900, fontSize: 40, fontFamily: 'SEBANG_Gothic_Bold', textAlign:'center'},
-  regular: {color: Colors.blue900, fontSize: 20, fontFamily: 'SEBANG_Gothic_Bold', textAlign:'center', paddingTop: 25, paddingLeft: 10},
+  container : { flex: 1, backgroundColor: '#ffffff', alignItems: 'center', justifyContent: 'center'},
 
-  textInput_1: {fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: 'white', borderWidth: 3, borderRadius: 10, 
-    padding: 10, marginLeft: '10%', marginRight: '10%', marginTop: 15, fontSize: 15, borderColor: Colors.blue500},
+  textInput_id: {fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: 'white', borderWidth: 1, borderRadius: 10,
+    padding: 10, marginLeft: 40, marginRight: 150, marginTop: 15, fontSize: 15, borderColor: "#D9D9D9"},
 
-  textInput_2: {fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: 'white', borderWidth: 3, borderRadius: 10, marginTop: 15, fontSize: 15,
-    width: '35%', padding: 10},
+  textInput_pw: {fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: 'white', borderWidth: 1, borderRadius: 10,
+    padding: 10, marginLeft: '10%', marginRight: '10%', marginTop: 5, fontSize: 15, borderColor: "#D9D9D9"},
 
-  button: {color: Colors.white, fontSize: 20, fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: Colors.blue500, 
+  textInput_name: {fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: 'white', borderWidth: 1, borderRadius: 10,
+    padding: 10, marginLeft: '10%', marginRight: '10%', marginTop: 15, fontSize: 15, borderColor: "#D9D9D9"},
+
+  textInput_email: {fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: 'white', borderWidth: 1, borderRadius: 10,
+    padding: 10, marginLeft: 40, marginRight: 150, marginTop: 15, fontSize: 15, borderColor: "#D9D9D9"},
+
+  button: {color: "#000000", fontSize: 20, fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: "#A1BFDD",
     borderRadius: 10, padding: 10, marginLeft: '10%', marginRight: '10%', marginTop: 15, textAlign: 'center'},
 
-  instruction: {fontFamily: 'SEBANG_Gothic_Bold', justifyContent: 'center', marginTop: 15, fontSize: 15, paddingHorizontal: 50},
+  button_IdCheck: {color: "#000000", fontSize: 20, fontFamily: 'SEBANG_Gothic_Bold', backgroundColor: "#D8E8F2",
+    borderRadius: 10, padding: 10, marginLeft: 200, marginRight: 10, marginTop: 15, textAlign: 'center'},
+
+  picker : {backgroundColor: "#ffffff", borderRadius: 10, borderWidth: 1,
+          borderColor: "#D9D9D9", height : 40, width: 150, marginLeft: 250, marginRight: 100, marginTop: 20},
+
+  instruction: {fontFamily: 'SEBANG_Gothic_Bold', justifyContent: 'center', marginTop: 5, fontSize: 10, paddingHorizontal: 50},
+  instruction_2: {fontFamily: 'SEBANG_Gothic_Bold', justifyContent: 'center', marginTop: 5, fontSize: 15, paddingHorizontal: 50}
 })
--- HELF/src/screens/start_screen.tsx
+++ HELF/src/screens/start_screen.tsx
@@ -1,25 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { MD2Colors as Colors } from 'react-native-paper'
+import {  StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { MD2Colors as Colors } from 'react-native-paper';
 
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
 
 export default function StartScreen({navigation} : any) {
+
     return (
         <View style={styles.view}>
-            <Text>시작 화면(로그인 화면)입니다.</Text>
+            <View>
+                <Image
+                source={require('../assets/ic_launcher.png')}
+                style={{ width: 300, height: 150}}
+                />
+            </View>
+
             <TouchableOpacity>
-                <Text style={styles.button} onPress={() => {
+                <Text style={styles.button_login} onPress={() => {
                     navigation.navigate('Home')
-            }}>GO TO HOME SCREEN</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text style={styles.button} onPress={() => {navigation.navigate('SignUp')}}>SIGN UP</Text>
+            }}>카카오 로그인</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    view : {justifyContent: 'center', flex: 1, alignItems: 'center'},
-    button : {color: Colors.white, fontSize: 20, backgroundColor: Colors.blue400, padding: 5, borderRadius: 10, fontFamily: 'SEBANG_Gothic'}
+    view : {backgroundColor: '#ffffff', justifyContent: 'center', flex: 1, alignItems: 'center'},
+    button_login : {color: '#000000', fontSize: 20, backgroundColor: '#fef01b', padding: 5, borderRadius: 10, width: 200, height: 30, fontFamily: 'SEBANG_Gothic',
+         justifyContent: 'center', marginTop: 40, textAlign: 'center'}
 })
